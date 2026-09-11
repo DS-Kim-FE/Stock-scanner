4b5d6c5410f94c3234b80c32c1e6950ffb05fd1c
DS-Kim-FE <kds2dae@bosch.com>
2026-09-11 11:24:02 +0200

    Update stockfind.py

--- stockfind.py
+++ stockfind.py
@@ -15,7 +15,7 @@ from datetime import datetime
 def get_headers():
     return {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'Referer': 'https://finance.naver.com/'
+        'Referer': 'https://stock.naver.com/'
     }
 
 def get_market_sum_pages(page_list, market="KOSPI"):
